fb67062036d84cd345d4183a6eca7ddb2f43e1ac
John H. Aughey <jha@aughey.com>
2024-12-02 15:33:22 +0000

    updates

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ use aoc_runner_derive::aoc_lib;
 
 aoc_lib! { year = 2024 }
 
+/// Read the problem sample test for a day from the input directory.
 pub fn test_data(day: u32) -> Result<String> {
     Ok(std::fs::read_to_string(format!(
         "input/2024/day{}-test.txt",
@@ -15,70 +16,62 @@ pub fn test_data(day: u32) -> Result<String> {
     ))?)
 }
 
-/// Sums the given iterator of results.
-/// This allows for early return of errors.
-fn sum_results<T: std::ops::Add<Output = T> + Default>(
-    iter: impl Iterator<Item = Result<T>>,
-) -> Result<T> {
-    let mut sum = T::default();
-    for v in iter {
-        sum = sum + v?;
+// checked_add functions on u16, i16, u32, i32, etc are not defined as a trait.
+// This is our own definition of checked_add that is implemented for a few types used
+// in the solutions.  Other types can be added as needed.
+pub trait CheckedAdd<T> {
+    fn checked_add(self, other: T) -> Option<T>;
+}
+impl CheckedAdd<u32> for u32 {
+    fn checked_add(self, rhs: u32) -> Option<u32> {
+        self.checked_add(rhs)
     }
-    Ok(sum)
+}
+impl CheckedAdd<usize> for usize {
+    fn checked_add(self, rhs: usize) -> Option<usize> {
+        self.checked_add(rhs)
+    }
 }
 
-// Create a mixin so that .sum_results() works on iterators of Results.
+/// Similar to the sum() function on iterators, but for results.
 pub trait SumResults<T> {
     fn sum_results(self) -> Result<T>;
 }
-pub struct SumResultsMixin<I>(I);
+
+/// blanket implementation for all iterators of results that add.
+/// This also checks the sum for overflow.
 impl<T, I> SumResults<T> for I
 where
     I: Iterator<Item = Result<T>>,
     T: std::ops::Add<Output = T> + Default,
+    T: CheckedAdd<T>,
 {
     fn sum_results(self) -> Result<T> {
-        sum_results(self)
+        let mut sum = T::default();
+        for v in self {
+            sum = sum
+                .checked_add(v?)
+                .ok_or_else(|| anyhow::anyhow!("add overflowed"))?;
+        }
+        Ok(sum)
     }
 }
 
+/// Similar to the sum() function on iterators, but will check for overflow of the sum itself.
 pub trait CheckedSum<T> {
     fn checked_sum(self) -> Result<T>;
 }
-impl<I> CheckedSum<u32> for I
+impl<T, I> CheckedSum<T> for I
 where
-    I: Iterator<Item = u32>,
+    I: Iterator<Item = T>,
+    T: std::ops::Add<Output = T> + Default + CheckedAdd<T>,
 {
-    fn checked_sum(self) -> Result<u32> {
-        let mut sum = 0u32;
+    fn checked_sum(self) -> Result<T> {
+        let mut sum = T::default();
         for v in self {
-            sum = sum + v;
-        }
-        Ok(sum)
-    }
-}
-
-impl<I> CheckedSum<i32> for I
-where
-    I: Iterator<Item = i32>,
-{
-    fn checked_sum(self) -> Result<i32> {
-        let mut sum = 0i32;
-        for v in self {
-            sum = sum + v;
-        }
-        Ok(sum)
-    }
-}
-
-impl<I> CheckedSum<usize> for I
-where
-    I: Iterator<Item = usize>,
-{
-    fn checked_sum(self) -> Result<usize> {
-        let mut sum = 0usize;
-        for v in self {
-            sum = sum + v;
+            sum = sum
+                .checked_add(v)
+                .ok_or_else(|| anyhow::anyhow!("add overflowed"))?;
         }
         Ok(sum)
     }
