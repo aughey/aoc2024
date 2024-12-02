1b7b1f16162a7721934b696a65a7a22a607ac236
John H. Aughey <jha@aughey.com>
2024-12-02 14:14:36 +0000

    Test script to make sure the project works for a given day

---
+++ test_codspeed.sh
@@ -0,0 +1,14 @@
+day=`echo $1 | sed 's/[^0-9]//g'`
+if [ -z "$day" ]; then
+    echo "Usage: $0 <day>"
+    exit 1
+fi
+
+codspeed=~/Advent/rust-runner
+
+cp input/2024/day$1.txt $codspeed/input.txt
+cd $codspeed
+rm Cargo.lock
+
+sed -i "s/day[0-9]*/day$day/g" bench.rs
+cargo codspeed build && cargo codspeed run
