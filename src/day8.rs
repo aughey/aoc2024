e27acfe12930fcc3dfd40d01d439420213a64a7a
John Aughey <jha@aughey.com>
2024-12-08 23:11:19 +0000

    need to leave well enough alone

--- src/day8.rs
+++ src/day8.rs
@@ -1,172 +1,24 @@
 use crate::Result;
-use anyhow::Context as _;
-use aoc_runner_derive::{aoc, aoc_generator};
-use std::{collections::HashSet, fmt::Display, str::FromStr};
-use tracing::info;
+use aoc_runner_derive::aoc;
+use day8_impl::{DataShape, Node, XY};
+use std::{fmt::Display, str::FromStr};
 
 pub const DAY: u32 = 8;
 
-trait XY {
-    fn xy(&self) -> &glam::IVec2;
-    fn frequency(&self) -> char;
-}
-
-/// Parsing logic uses the FromStr trait
-#[aoc_generator(day8)]
-fn parse(input: &str) -> Result<Data> {
-    info!("Parsing input");
-    Data::from_str(input).context("input parsing")
-}
-
-/// Solution to part 1
 #[aoc(day8, part1)]
-fn solve_part1(input: &impl DataShape) -> Result<usize> {
-    let max_xy = &input.max_xy();
-
-    let antinodes = input.resonate_pairs().map(|ab| {
-        let (a, b) = ab?;
-        // We skip the first node because it's the same as the second node.
-        // We only take 1 node because part one only considers the first antinode.
-        let forward_locations = anitnode_generator(&a, &b).skip(1).take(1);
-        let backward_locations = anitnode_generator(&b, &a).skip(1).take(1);
-
-        // neat little trick to capture max_xy so take_while looks clean
-        let on_map = |node: &Node| on_map(node, max_xy);
-
-        let valid_forward_locations = forward_locations.take_while(on_map);
-        let valid_backward_locations = backward_locations.take_while(on_map);
-
-        Ok::<_, anyhow::Error>(valid_forward_locations.chain(valid_backward_locations))
-    });
-
-    // Because of the inner result, we need to unwrap the inner result manually.
-    // There should be a flat_map_results or something similar in the future.
-    let mut antinode_positions = HashSet::new();
-    for antinode in antinodes {
-        let antinode = antinode?;
-        antinode_positions.extend(antinode.map(|n| n.xy));
-    }
-    // antinodes.map(|node| Ok(node?.map(|n| n.xy)).collect::<Result<HashSet<_>>>()?;
-
-    Ok(antinode_positions.len())
-}
-
-/// Generates antinodes for a given pair of nodes in the direction of a->b
-///
-/// Given input that looks like `a - b`,
-/// it will generate * nodes `a - * - * - *....`
-/// where * are the new nodes geneated and includes node b.
-///
-/// If you want it in the other direction, call it with `b` and `a` instead.
-fn anitnode_generator(a: &impl XY, b: &impl XY) -> impl Iterator<Item = Node> {
-    let diff = b.xy() - a.xy();
-    let xy = *b.xy();
-    let frequency = b.frequency();
-    (0..)
-        // compute the new xy
-        .map(move |i| xy + diff * i)
-        // create the new node
-        .map(move |xy| Node { frequency, xy })
-}
-
-/// Checks if a node is within the bounds of the map bounds
-fn on_map(node: &Node, max_xy: &glam::IVec2) -> bool {
-    node.xy.x >= 0 && node.xy.y >= 0 && node.xy.x < max_xy.x && node.xy.y < max_xy.y
+fn solve_part1(input: &str) -> Result<usize> {
+    let input = Data::from_str(input)?;
+    day8_impl::solve_part1_impl(&input)
 }
 
 /// Solution to part 2
 #[aoc(day8, part2)]
-fn solve_part2(input: &impl DataShape) -> Result<usize> {
-    let max_xy = &input.max_xy();
-
-    let antinodes = input.resonate_pairs().map(|ab| {
-        let (a, b) = ab?;
-        let forward_locations = anitnode_generator(&a, &b);
-        let backward_locations = anitnode_generator(&b, &a);
-
-        // neat little trick
-        let on_map = |node: &Node| on_map(node, max_xy);
-
-        let valid_forward_locations = forward_locations.take_while(on_map);
-        let valid_backward_locations = backward_locations.take_while(on_map);
-
-        Ok::<_, anyhow::Error>(valid_forward_locations.chain(valid_backward_locations))
-    });
-
-    let mut antinode_positions = HashSet::new();
-    for antinode in antinodes {
-        let antinode = antinode?;
-        antinode_positions.extend(antinode.map(|n| n.xy));
-    }
-    info!("antinode_positions: {:?}", antinode_positions);
-
-    Ok(antinode_positions.len())
+fn solve_part2(input: &str) -> Result<usize> {
+    let input = Data::from_str(input)?;
+    day8_impl::solve_part2_impl(&input)
 }
 
-#[derive(Debug, Clone)]
-struct Node {
-    frequency: char,
-    xy: glam::IVec2,
-}
-impl XY for Node {
-    fn xy(&self) -> &glam::IVec2 {
-        &self.xy
-    }
-
-    fn frequency(&self) -> char {
-        self.frequency
-    }
-}
-
-impl XY for &Node {
-    fn xy(&self) -> &glam::IVec2 {
-        &self.xy
-    }
-
-    fn frequency(&self) -> char {
-        self.frequency
-    }
-}
-
-/// Problem input
-#[derive(Debug)]
-struct Data {
-    nodes: Vec<Node>,
-    max_xy: glam::IVec2,
-}
-
-/// Generates all pairs of a given iterator.
-/// This is equivalent to `iter.combinations(2)`.
-fn pair_combinations<T, IT>(iter: IT) -> impl Iterator<Item = (T, T)>
-where
-    T: Clone,
-    IT: Iterator<Item = T> + Clone,
-{
-    let mut a = iter;
-    let mut left = a.next();
-    let mut b = a.clone();
-    std::iter::from_fn(move || loop {
-        match (left.clone(), b.next()) {
-            (Some(left), Some(right)) => return Some((left, right)),
-            (Some(_), None) => {
-                left = Some(a.next()?);
-                b = a.clone();
-            }
-            _ => return None,
-        }
-    })
-}
-
-trait DataShape {
-    type RPNODE<'a>: XY
-    where
-        Self: 'a;
-    fn resonate_pairs<'a>(
-        &'a self,
-    ) -> impl Iterator<Item = Result<(Self::RPNODE<'a>, Self::RPNODE<'a>)>> + 'a;
-
-    fn max_xy(&self) -> glam::IVec2;
-}
+mod day8_impl;
 
 impl DataShape for Data {
     type RPNODE<'a>
@@ -190,48 +42,109 @@ impl DataShape for Data {
         //     .map(Ok)
     }
 
-    fn max_xy(&self) -> glam::IVec2 {
-        self.max_xy
+    fn max_xy(&self) -> Result<glam::IVec2> {
+        Ok(self.max_xy)
     }
+}
+
+fn parse_nodes(s: &str) -> impl Iterator<Item = Result<Node>> + '_ + Clone {
+    s.lines().enumerate().flat_map(|(y, line)| {
+        line.chars()
+            .enumerate()
+            .filter(|(_, c)| c != &'.')
+            .map(move |(x, c)| {
+                Ok(Node {
+                    frequency: c,
+                    xy: glam::IVec2::new(x.try_into()?, y.try_into()?),
+                })
+            })
+    })
+}
+
+fn parse_maxxy(s: &str) -> Result<glam::IVec2> {
+    let max_y = s.lines().count();
+    let max_x = s.lines().next().unwrap().len();
+    Ok(glam::IVec2::new(max_x.try_into()?, max_y.try_into()?))
 }
 
 impl FromStr for Data {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self> {
-        let s = s.lines();
-
-        let nodes = s.clone().enumerate().flat_map(|(y, line)| {
-            line.chars()
-                .enumerate()
-                .filter(|(_, c)| c != &'.')
-                .map(move |(x, c)| {
-                    Ok(Node {
-                        frequency: c,
-                        xy: glam::IVec2::new(x.try_into()?, y.try_into()?),
-                    })
-                })
-        });
-        //   .collect::<Result<Vec<_>>>()?;
-
-        let max_y = s.clone().count();
-        let max_x = s.clone().next().unwrap().len();
-
-        Ok(Data {
-            nodes: nodes.collect::<Result<Vec<_>>>()?,
-            max_xy: glam::IVec2::new(max_x.try_into()?, max_y.try_into()?),
+        Ok(Self {
+            nodes: parse_nodes(s).collect::<Result<_>>()?,
+            max_xy: parse_maxxy(s)?,
         })
     }
 }
 
 /// codspeed compatible function
 pub fn part1(input: &str) -> impl Display {
-    solve_part1(&parse(input).unwrap()).unwrap()
+    solve_part1(input).unwrap()
 }
 
 /// codspeed compatible function
 pub fn part2(input: &str) -> impl Display {
-    solve_part2(&parse(input).unwrap()).unwrap()
+    solve_part2(input).unwrap()
+}
+
+/// Problem input
+#[derive(Debug)]
+pub struct Data {
+    pub nodes: Vec<Node>,
+    pub max_xy: glam::IVec2,
+}
+
+pub struct DataNoStd<'a> {
+    pub s: &'a str,
+}
+
+impl<'a> DataNoStd<'a> {
+    pub fn new(s: &'a str) -> Self {
+        Self { s }
+    }
+}
+
+impl DataShape for DataNoStd<'_> {
+    type RPNODE<'a>
+        = Node
+    where
+        Self: 'a;
+    fn resonate_pairs<'a>(
+        &'a self,
+    ) -> impl Iterator<Item = Result<(Self::RPNODE<'a>, Self::RPNODE<'a>)>> + 'a {
+        let nodes = parse_nodes(self.s);
+        let nodes = nodes.map(|n| n.unwrap());
+        pair_combinations(nodes)
+            .filter(|(a, b)| a.frequency == b.frequency)
+            .map(Ok)
+    }
+
+    fn max_xy(&self) -> Result<glam::IVec2> {
+        parse_maxxy(self.s)
+    }
+}
+
+/// Generates all pairs of a given iterator.
+/// This is equivalent to `iter.combinations(2)`.
+pub fn pair_combinations<T, IT>(iter: IT) -> impl Iterator<Item = (T, T)> + Clone
+where
+    T: Clone,
+    IT: Iterator<Item = T> + Clone,
+{
+    let mut a = iter;
+    let mut left = a.next();
+    let mut b = a.clone();
+    std::iter::from_fn(move || loop {
+        match (left.clone(), b.next()) {
+            (Some(left), Some(right)) => return Some((left, right)),
+            (Some(_), None) => {
+                left = Some(a.next()?);
+                b = a.clone();
+            }
+            _ => return None,
+        }
+    })
 }
 
 #[cfg(test)]
@@ -243,18 +156,12 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        assert_eq!(
-            solve_part1(&parse(&test_data(super::DAY).unwrap()).unwrap()).unwrap(),
-            14
-        );
+        assert_eq!(solve_part1(&test_data(super::DAY).unwrap()).unwrap(), 14);
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(
-            solve_part2(&parse(&test_data(super::DAY).unwrap()).unwrap()).unwrap(),
-            34
-        );
+        assert_eq!(solve_part2(&test_data(super::DAY).unwrap()).unwrap(), 34);
     }
 
     #[test]
---
+++ src/day8/day8_impl.rs
@@ -0,0 +1,127 @@
+// Technically the impl is no_std
+use crate::Result;
+use std::collections::HashSet;
+
+#[derive(Debug, Clone)]
+pub struct Node {
+    pub frequency: char,
+    pub xy: glam::IVec2,
+}
+
+impl XY for Node {
+    fn xy(&self) -> &glam::IVec2 {
+        &self.xy
+    }
+
+    fn frequency(&self) -> char {
+        self.frequency
+    }
+}
+
+impl XY for &Node {
+    fn xy(&self) -> &glam::IVec2 {
+        &self.xy
+    }
+
+    fn frequency(&self) -> char {
+        self.frequency
+    }
+}
+
+pub trait XY {
+    fn xy(&self) -> &glam::IVec2;
+    fn frequency(&self) -> char;
+}
+
+pub fn solve_part1_impl(input: &impl DataShape) -> Result<usize> {
+    let max_xy = input.max_xy();
+    let max_xy = &max_xy?;
+
+    let antinodes = input.resonate_pairs().map(|ab| {
+        let (a, b) = ab?;
+        // We skip the first node because it's the same as the second node.
+        // We only take 1 node because part one only considers the first antinode.
+        let forward_locations = anitnode_generator(&a, &b).skip(1).take(1);
+        let backward_locations = anitnode_generator(&b, &a).skip(1).take(1);
+
+        // neat little trick to capture max_xy so take_while looks clean
+        let on_map = |node: &Node| on_map(node, max_xy);
+
+        let valid_forward_locations = forward_locations.take_while(on_map);
+        let valid_backward_locations = backward_locations.take_while(on_map);
+
+        Ok::<_, anyhow::Error>(valid_forward_locations.chain(valid_backward_locations))
+    });
+
+    // Because of the inner result, we need to unwrap the inner result manually.
+    // There should be a flat_map_results or something similar in the future.
+    let mut antinode_positions = HashSet::new();
+    for antinode in antinodes {
+        let antinode = antinode?;
+        antinode_positions.extend(antinode.map(|n| n.xy));
+    }
+    // antinodes.map(|node| Ok(node?.map(|n| n.xy)).collect::<Result<HashSet<_>>>()?;
+
+    Ok(antinode_positions.len())
+}
+
+/// Generates antinodes for a given pair of nodes in the direction of a->b
+///
+/// Given input that looks like `a - b`,
+/// it will generate * nodes `a - * - * - *....`
+/// where * are the new nodes geneated and includes node b.
+///
+/// If you want it in the other direction, call it with `b` and `a` instead.
+pub fn anitnode_generator(a: &impl XY, b: &impl XY) -> impl Iterator<Item = Node> {
+    let diff = b.xy() - a.xy();
+    let xy = *b.xy();
+    let frequency = b.frequency();
+    (0..)
+        // compute the new xy
+        .map(move |i| xy + diff * i)
+        // create the new node
+        .map(move |xy| Node { frequency, xy })
+}
+
+/// Checks if a node is within the bounds of the map bounds
+pub fn on_map(node: &Node, max_xy: &glam::IVec2) -> bool {
+    node.xy.x >= 0 && node.xy.y >= 0 && node.xy.x < max_xy.x && node.xy.y < max_xy.y
+}
+
+pub fn solve_part2_impl(input: &impl DataShape) -> Result<usize> {
+    let max_xy = input.max_xy();
+    let max_xy = &max_xy?;
+
+    let antinodes = input.resonate_pairs().map(|ab| {
+        let (a, b) = ab?;
+        let forward_locations = anitnode_generator(&a, &b);
+        let backward_locations = anitnode_generator(&b, &a);
+
+        // neat little trick
+        let on_map = |node: &Node| on_map(node, max_xy);
+
+        let valid_forward_locations = forward_locations.take_while(on_map);
+        let valid_backward_locations = backward_locations.take_while(on_map);
+
+        Ok::<_, anyhow::Error>(valid_forward_locations.chain(valid_backward_locations))
+    });
+
+    let mut antinode_positions = HashSet::new();
+    for antinode in antinodes {
+        let antinode = antinode?;
+        antinode_positions.extend(antinode.map(|n| n.xy));
+    }
+
+    Ok(antinode_positions.len())
+}
+
+pub trait DataShape {
+    type RPNODE<'a>: XY
+    where
+        Self: 'a;
+    fn resonate_pairs<'a>(
+        &'a self,
+    ) -> impl Iterator<Item = Result<(Self::RPNODE<'a>, Self::RPNODE<'a>)>> + 'a;
+
+    fn max_xy(&self) -> Result<glam::IVec2>;
+}
