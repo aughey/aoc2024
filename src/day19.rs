98016673ee280a216a985ce17476962da1d94392
John Aughey <jha@aughey.com>
2024-12-19 05:53:05 +0000

    day 19 complete (easy)

---
+++ src/day19.rs
@@ -0,0 +1,109 @@
+use crate::Result;
+use anyhow::Context as _;
+use aoc_runner_derive::aoc;
+use std::fmt::Display;
+
+pub const DAY: u32 = 19;
+
+fn possible_counts(pattern: &str, towels: &[impl AsRef<str>]) -> usize {
+    let pattern = pattern.to_string();
+    pathfinding::directed::count_paths::count_paths(
+        pattern,
+        |p| {
+            towels
+                .iter()
+                .map(|d| d.as_ref())
+                .filter_map(|d| Some((p.get(..d.len())?, d)))
+                .filter(|(p, d)| p == d)
+                .map(|(_, d)| p.get(d.len()..).unwrap())
+                .map(|p| p.to_string())
+                .collect::<Vec<_>>()
+        },
+        |p| p.is_empty(),
+    )
+}
+
+fn solve_part1_impl(input: &Data) -> Result<usize> {
+    Ok(input
+        .patterns
+        .iter()
+        .filter(|p| possible_counts(p, &input.towels) > 0)
+        .count())
+}
+
+fn solve_part2_impl(input: &Data) -> Result<usize> {
+    Ok(input
+        .patterns
+        .iter()
+        .map(|p| possible_counts(p, &input.towels))
+        .sum())
+}
+
+/// Solution to part 1
+#[aoc(day19, part1)]
+fn solve_part1(input: &str) -> Result<usize> {
+    let input = Data::parse(input).context("input parsing")?;
+    solve_part1_impl(&input)
+}
+
+/// Solution to part 2
+#[aoc(day19, part2)]
+fn solve_part2(input: &str) -> Result<usize> {
+    let input = Data::parse(input).context("input parsing")?;
+    solve_part2_impl(&input)
+}
+
+/// Problem input
+#[derive(Debug)]
+struct Data {
+    towels: Vec<String>,
+    patterns: Vec<String>,
+}
+impl Data {
+    fn parse(s: &str) -> Result<Self> {
+        let (towels, patterns) = s
+            .split_once("\n\n")
+            .ok_or_else(|| anyhow::anyhow!("Invalid input split"))?;
+
+        let towels = towels
+            .split(',')
+            .map(|s| s.trim())
+            .map(|s| s.to_string())
+            .collect();
+        let patterns = patterns
+            .split('\n')
+            .map(|s| s.trim())
+            .map(|s| s.to_string())
+            .collect();
+
+        Ok(Data { towels, patterns })
+    }
+}
+
+/// codspeed compatible function
+pub fn part1(input: &str) -> impl Display {
+    solve_part1(input).unwrap()
+}
+
+/// codspeed compatible function
+pub fn part2(input: &str) -> impl Display {
+    solve_part2(input).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::test_data;
+    use test_log::test;
+
+    use super::*;
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(solve_part1(&test_data(super::DAY).unwrap()).unwrap(), 6);
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(solve_part2(&test_data(super::DAY).unwrap()).unwrap(), 16);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ pub mod day16;
 pub mod day17;
 pub mod day18;
 pub mod day18_old;
+pub mod day19;
 pub mod day2;
 pub mod day3;
 pub mod day4;
