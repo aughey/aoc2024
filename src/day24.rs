c21db33dfb95d17a521782264b845027b2e3aa10
John Aughey <jha@aughey.com>
2024-12-25 14:38:23 +0000

    ADS

--- src/day24.rs
+++ src/day24.rs
@@ -1,24 +1,61 @@
 use crate::Result;
 use anyhow::Context as _;
 use aoc_runner_derive::aoc;
-use std::{collections::HashMap, fmt::Display};
+use itertools::Itertools as _;
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::Display,
+};
 
 pub const DAY: u32 = 24;
+
+fn sort_operations<'a>(start: &[&str], operations: &[Operation<'a>]) -> Result<Vec<Operation<'a>>> {
+    // topological sort of operations
+    let indicies = (0..(start.len() + operations.len())).collect::<Vec<_>>();
+    let sorting =
+        pathfinding::directed::topological_sort::topological_sort(indicies.as_slice(), |&i| {
+            let output = if i < start.len() {
+                start[i]
+            } else {
+                let op = &operations[i - start.len()];
+                op.dest
+            };
+            operations
+                .iter()
+                .enumerate()
+                .filter(move |(_, op)| op.a == output || op.b == output)
+                .map(|(i, _)| i + start.len())
+        })
+        .map_err(|e| anyhow::anyhow!("topological sort failed: {:?}", e))?;
+    Ok(sorting
+        .into_iter()
+        .filter_map(|i| i.checked_sub(start.len()))
+        .map(|i| operations[i].clone())
+        .collect())
+}
 
 fn solve_part1_impl(input: &Data) -> Result<usize> {
     let mut state = input.start.clone();
 
-    loop {
-        let mut missed_one = false;
-        for op in &input.operations {
-            if op.try_op(&mut state).is_none() {
-                missed_one = true;
-            }
-        }
-        if !missed_one {
-            break;
-        }
+    let start = input.start.keys().copied().collect::<Vec<_>>();
+
+    let operations = sort_operations(start.as_slice(), input.operations.as_slice())?;
+
+    {
+        // make sure all outputs are unique
+        let op_outputs = operations.iter().map(|op| op.dest).collect::<HashSet<_>>();
+        assert_eq!(op_outputs.len(), operations.len());
     }
+
+    evaluate_z(operations.as_slice(), &mut state, &[])
+}
+
+fn evaluate_z<'a>(
+    operations: &'a [Operation],
+    state: &mut State<'a>,
+    swaps: &[(&'a str, &'a str)],
+) -> Result<usize> {
+    evaluate_ops(operations, state, swaps)?;
 
     let mut result = 0usize;
     for i in 0..64 {
@@ -28,13 +65,131 @@ fn solve_part1_impl(input: &Data) -> Result<usize> {
             result |= (value as usize) << i;
         }
     }
-
     Ok(result)
 }
 
-fn solve_part2_impl(_input: &Data) -> Result<usize> {
-    // XXX: Solving logic for part 2
+fn evaluate_ops<'a>(
+    operations: &'a [Operation],
+    state: &mut State<'a>,
+    swaps: &[(&'a str, &'a str)],
+) -> Result<()> {
+    for op in operations {
+        op.try_op(state, swaps).ok_or_else(|| {
+            anyhow::anyhow!("failed to evaluate operation: {op:?} with swaps {swaps:?}")
+        })?;
+    }
+    Ok(())
+}
+
+fn solve_part2_impl(input: &Data) -> Result<usize> {
+    let mut state = input.start.clone();
+
+    let start = input.start.keys().copied().collect::<Vec<_>>();
+
+    let operations = sort_operations(start.as_slice(), input.operations.as_slice())?;
+
+    // // output operations as a mermaid graph
+    // println!("graph TD;");
+    // for (i, op) in operations.iter().enumerate() {
+    //     println!("    {}({:?})", i, op.op);
+    //     println!("    {} --> {}", i, op.dest);
+    //     println!("    {} --> {}", op.a, i);
+    //     println!("    {} --> {}", op.b, i);
+    // }
+
+    {
+        // make sure all outputs are unique
+        let op_outputs = operations.iter().map(|op| op.dest).collect::<HashSet<_>>();
+        assert_eq!(op_outputs.len(), operations.len());
+    }
+
+    let outputs = operations.iter().map(|op| op.dest).collect::<Vec<_>>();
+
+    // Make unique indicies of outputs
+    let first_indicies = (0..outputs.len()).flat_map(|ax| {
+        ((ax + 1)..outputs.len())
+            .filter(move |&bx| ax != bx)
+            .map(move |bx| (ax, bx))
+    });
+    fn pair_single_equality(&(a, b): &(usize, usize), other: usize) -> bool {
+        a == other || b == other
+    }
+    fn pair_pair_equality(&(a, b): &(usize, usize), &(c, d): &(usize, usize)) -> bool {
+        a == c || a == d || b == c || b == d
+    }
+    let second_indices = first_indicies.flat_map(|first| {
+        ((first.1 + 1)..outputs.len())
+            .filter(move |ax| !pair_single_equality(&first, *ax))
+            .flat_map(|ax| ((ax + 1)..outputs.len()).map(move |bx| (ax, bx)))
+            .filter(move |second| !pair_pair_equality(&first, second))
+            .map(move |second| (first, second))
+    });
+    let third_indicies = second_indices.flat_map(|(first, second)| {
+        ((second.1 + 1)..outputs.len())
+            .filter(move |ax| {
+                !pair_single_equality(&first, *ax) && !pair_single_equality(&second, *ax)
+            })
+            .flat_map(|ax| ((ax + 1)..outputs.len()).map(move |bx| (ax, bx)))
+            .filter(move |third| {
+                !pair_pair_equality(&first, third) && !pair_pair_equality(&second, third)
+            })
+            .map(move |third| (first, second, third))
+    });
+    let fourth_indices = third_indicies.flat_map(|(first, second, third)| {
+        ((third.1 + 1)..outputs.len())
+            .filter(move |ax| {
+                !pair_single_equality(&first, *ax)
+                    && !pair_single_equality(&second, *ax)
+                    && !pair_single_equality(&third, *ax)
+            })
+            .flat_map(|ax| ((ax + 1)..outputs.len()).map(move |bx| (ax, bx)))
+            .filter(move |fourth| {
+                !pair_pair_equality(&first, fourth)
+                    && !pair_pair_equality(&second, fourth)
+                    && !pair_pair_equality(&third, fourth)
+            })
+            .map(move |fourth| (first, second, third, fourth))
+    });
+
+    println!(
+        "number of wierd indicies things we have about is {}",
+        fourth_indices.clone().count()
+    );
+    println!(
+        "First sequences are: {:?}",
+        fourth_indices.take(5).collect::<Vec<_>>()
+    );
+
+    let output_pairs = outputs
+        .into_iter()
+        .combinations(2)
+        .map(|pair| (pair[0], pair[1]))
+        .collect::<Vec<_>>();
+
+    println!("number of output pairs: {}", output_pairs.len());
+
+    // Now all combinations of 4 of these output pairs
+    set_bits(5, &mut state, "x");
+    set_bits(6, &mut state, "y");
+    const EXPECTED: usize = 11;
+    for swap_combo in output_pairs.into_iter().combinations(4) {
+        let res = evaluate_z(operations.as_slice(), &mut state, swap_combo.as_slice())?;
+        if res == EXPECTED {
+            println!("found solution: {:?}", swap_combo);
+            break;
+        }
+    }
     Ok(0)
+}
+
+fn set_bits(value: u64, state: &mut State, prefix: &str) {
+    for i in 0..64 {
+        let bit = (value >> i) & 1;
+        let key = format!("{}{:02}", prefix, i);
+        if let Some(cur) = state.get_mut(key.as_str()) {
+            *cur = bit as u8;
+        }
+    }
 }
 
 /// Solution to part 1
@@ -53,7 +208,7 @@ fn solve_part2(input: &str) -> Result<usize> {
 
 type State<'a> = HashMap<&'a str, u8>;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 enum Op {
     XOR,
     OR,
@@ -71,7 +226,7 @@ impl TryFrom<&str> for Op {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Operation<'a> {
     op: Op,
     a: &'a str,
@@ -79,7 +234,7 @@ struct Operation<'a> {
     dest: &'a str,
 }
 impl<'a> Operation<'a> {
-    fn try_op(&self, state: &mut State<'a>) -> Option<()> {
+    fn try_op(&self, state: &mut State<'a>, swaps: &[(&'a str, &'a str)]) -> Option<()> {
         let a = state.get(self.a)?;
         let b = state.get(self.b)?;
         let result = match self.op {
@@ -87,7 +242,20 @@ impl<'a> Operation<'a> {
             Op::OR => a | b,
             Op::XOR => a ^ b,
         };
-        state.insert(self.dest, result);
+        let dest = swaps
+            .iter()
+            .copied()
+            .find_map(|(a, b)| {
+                if self.dest == a {
+                    Some(b)
+                } else if self.dest == b {
+                    Some(a)
+                } else {
+                    None
+                }
+            })
+            .unwrap_or(self.dest);
+        state.insert(dest, result);
         Some(())
     }
 }
@@ -140,7 +308,6 @@ impl<'a> Data<'a> {
             })
             .collect::<Result<_>>()?;
 
-        // XXX: Update the returned Data to include the parsed data.
         Ok(Data { operations, start })
     }
 }
